603356a892025196903ae6fbbc1f990e07815b32
schwadan001 <schwadan001@gmail.com>
2022-01-05 14:53:46 -0600

    increase use of contours

--- identify_flight.py
+++ identify_flight.py
@@ -1,4 +1,5 @@
 import cv2
+import math
 import numpy as np
 
 
@@ -34,35 +35,15 @@ def get_object_img(img, obj_arr):
     return cv2.bitwise_and(img, img, mask=refined_mask)
 
 
-def has_obj(obj_arr):
-    max_y, max_x = obj_arr.shape
-    for y in range(max_y):
-        if max(obj_arr[y]) != 0:
-            return True
-    return False
-
-
-def has_partial_object(obj_arr):
-    max_y, max_x = obj_arr.shape
-    for y in range(max_y):
-        if obj_arr[y, width-1] > 0 or obj_arr[y, 0] > 0:
-            return True
-    return False
-
-
-def get_obj_diameter(obj_arr):
-    o = obj_arr.transpose()
-    max_y, max_x = o.shape
-    obj_start, obj_end = 0, max_y
-    start_acquired = False
-    for y in range(max_y):
-        if max(o[y]) > 0 and not start_acquired:
-            obj_start = y
-            start_acquired = True
-        elif max(o[y]) == 0 and start_acquired:
-            obj_end = y
-            break
-    return obj_end - obj_start
+def has_full_obj(circle):
+    if circle == None:
+        return False
+    (x,y), radius = circle
+    if x - radius < 0 or x + radius > width:
+        return False
+    if y - radius < 0 or y + radius > height:
+        return False
+    return True
           
 
 video = cv2.VideoCapture("{}.mp4".format(video_name))
@@ -74,7 +55,10 @@ size = (width, height)
 success, img = video.read()
 frames = []
 obj_arrs = []
+circles = []
 
+
+# create images from video
 print("Creating images...")
 
 count = 0
@@ -89,68 +73,58 @@ while success:
     success, img = video.read()
     count += 1
 
-print("Creating avi video...")
 
-out_format = cv2.VideoWriter_fourcc(*'DIVX')
-out = cv2.VideoWriter("{}_processed.avi".format(video_name), out_format, fps, size)
-out_slow = cv2.VideoWriter("{}_processed_slow.avi".format(video_name), out_format, fps / 8, size)
-for f in frames:
-    out.write(f)
-    out_slow.write(f)
-out.release()
-out_slow.release()
+# create video
+if True:
+    print("Creating avi video...")
+    out_format = cv2.VideoWriter_fourcc(*'DIVX')
+    out = cv2.VideoWriter("{}_processed.avi".format(video_name), out_format, fps, size)
+    out_slow = cv2.VideoWriter("{}_processed_slow.avi".format(video_name), out_format, fps / 8, size)
+    for f in frames:
+        out.write(f)
+        out_slow.write(f)
+    out.release()
+    out_slow.release()
 
-print("Projecting flight path...")
 
-frames_with_obj = []
-for i in range(len(obj_arrs)):
-    if has_obj(obj_arrs[i]):
-        frames_with_obj.append(i)
+# add disc tracking to frames
+print("Disc tracking...")
 
-full_img_frames = [i for i in frames_with_obj if not has_partial_object(obj_arrs[i])]
-
-first_partial_width = get_obj_diameter(obj_arrs[min(frames_with_obj)])
-last_partial_width = get_obj_diameter(obj_arrs[max(frames_with_obj)])
-obj_width = get_obj_diameter(obj_arrs[full_img_frames[0]])
-movement_frames = max(frames_with_obj) - min(frames_with_obj)
-
-# forward-moving velocity (configured for right-to-left)
-x_movement_pixels = width + obj_width - first_partial_width - last_partial_width
-x_pixels_per_frame = x_movement_pixels / movement_frames
-x_pixels_per_second = x_pixels_per_frame * fps
-x_feet_per_second = x_pixels_per_second / width * img_width_feet
-
-# side-moving velocity (up is negative, down is positive)
-first_partial_y_min, last_partial_y_min = height, height
-first_partial_y_max, last_partial_y_max = 0, 0
-for y in range(height):
-    if obj_arrs[min(frames_with_obj)][y, width-1] != 0:
-        first_partial_y_min = min(first_partial_y_min, y)
-        first_partial_y_max = max(first_partial_y_max, y)
-    if obj_arrs[max(frames_with_obj)][y, 0] != 0:
-        last_partial_y_min = min(last_partial_y_min, y)
-        last_partial_y_max = max(last_partial_y_max, y)
-
-first_partial_y_center = (first_partial_y_max + first_partial_y_min) // 2
-last_partial_y_center = (last_partial_y_max + last_partial_y_min) // 2
-
-y_movement_pixels = last_partial_y_center - first_partial_y_center
-y_pixels_per_frame = y_movement_pixels / movement_frames
-y_pixels_per_second = y_pixels_per_frame * fps
-y_feet_per_second = y_pixels_per_second / width * img_width_feet
-
-#########
-if False:
-    img = cv2.imread("frames/frame_20.jpg")
+for img in frames:
     img_bw = cv2.cvtColor(img.copy(), cv2.COLOR_BGR2GRAY)
     ret, thresh = cv2.threshold(img_bw, 127, 255, cv2.THRESH_BINARY)
-    contours, hier = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contour, hier = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     try:
-        c = contours[-1]
-        (x,y), radius = cv2.minEnclosingCircle(c)
+        circles.append(cv2.minEnclosingCircle(contour[-1]))
+    except Exception:
+        circles.append(None)
+
+frames_with_obj = [i for i in range(len(circles)) if has_full_obj(circles[i])]
+
+
+# velocity calcs
+feet_per_pixel = img_width_feet / width
+
+first_frame = frames_with_obj[0]
+last_frame = frames_with_obj[-1]
+movement_frames = last_frame - first_frame
+
+x_movement = (circles[last_frame][0][0] - circles[first_frame][0][0])
+y_movement = (circles[last_frame][0][1] - circles[first_frame][0][1])
+
+total_movement = math.sqrt((x_movement ** 2) + (y_movement ** 2))
+total_velocity = total_movement / movement_frames
+
+pixels_per_second = total_velocity * fps
+feet_per_second = pixels_per_second * feet_per_pixel
+
+# see images with disc tracking
+if True:
+    for i in frames_with_obj:
+        (x,y), radius = circles[i]
         center = (int(x), int(y))
         radius = int(radius)
-        img = cv2.circle(img, center, radius, green, 2)
-    except IndexError:
-        pass
-    cv2.imshow('img', img)
+        img = cv2.circle(cv2.imread("frames/frame_{}.jpg".format(i)), center, radius, green, 2)
+        cv2.imshow("Disc Tracking", img)
+        cv2.waitKey()
+    cv2.destroyAllWindows()
